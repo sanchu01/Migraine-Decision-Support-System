f2aabfaadd3d212ae3ded2ae41000403170cfaf3
Sanchez Innocencia D <60192477+sanchu01@users.noreply.github.com>
2022-11-07 00:54:30 +0530

    Update 5_🌲_Diagnosis Support Module.py

--- pages/5_🌲_Diagnosis Support Module.py
+++ pages/5_🌲_Diagnosis Support Module.py
@@ -10,7 +10,6 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import OneHotEncoder,StandardScaler
 from sklearn.tree import plot_tree
 from sklearn import tree
-import graphviz
 from dtreeviz.trees import *
 st.title('CLINICAL DECISION SUPPORT SYSTEM FOR MIGRAINE')
  
